85b65f728f91a6091b4931b2dfdd905a714ac441
Utsav Patel <utsav.kalash131297@gmail.com>
2021-10-31 14:36:25 -0400

    Bug fix.

--- src/Agent6.py
+++ src/Agent6.py
@@ -21,8 +21,7 @@ class Agent6(Agent):
         self.num_astar_calls += 1
         self.num_bumps += 1
         current_path, num_backtracks = forward_execution(self.maze, full_maze, self.current_position,
-                                                         self.current_estimated_goal, self.parents,
-                                                         want_to_explore_field_of_view=False)[:2]
+                                                         self.current_estimated_goal, self.parents)[:2]
         self.current_position = current_path[-1]
         self.final_paths.append(current_path)
         self.num_backtracks += num_backtracks
