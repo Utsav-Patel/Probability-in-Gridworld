Removed redundant argument. Corrected examination calculation. Dumped final output to pickle file.

helpers/helper.py
@@ -120,8 +120,8 @@ def parent_to_child_dict(parent: dict, starting_position: tuple):
 
 def generate_target_position(full_maze: list):
     while True:
-        x = random.randint(0, len(full_maze) - 1)
-        y = random.randint(0, len(full_maze) - 1)
+        x = random.randint(0, NUM_ROWS - 1)
+        y = random.randint(0, NUM_COLS - 1)
         if full_maze[x][y] == 1:
             continue
         return x, y
@@ -200,7 +200,7 @@ def length_of_path_from_source_to_goal(maze_array: np.array, start_pos: tuple, g
     return distance_array[goal_pos[0]][goal_pos[1]]
 
 
-def compute_current_estimated_goal(maze, current_pos, num_of_cells_processed, agent=6):
+def compute_current_estimated_goal(maze, current_pos, agent=6):
     # if num_of_cells_processed < 1:
     #     return current_pos
 

src/Agent6.py
@@ -21,9 +21,10 @@ class Agent6(Agent):
         self.final_paths.append(current_path)
         self.num_backtracks += num_backtracks
 
-    def examine(self, full_maze: np.array, target_pos):
+    def examine(self, target_pos):
         children = parent_to_child_dict(self.parents, self.current_estimated_goal)
         value = examine_and_propagate_probability(self.maze, self.current_position, target_pos,
                                                   self.current_estimated_goal, children[self.current_position])
-        self.num_examinations += 1
+        if self.current_position == self.current_estimated_goal:
+            self.num_examinations += 1
         return value

tests/agent6.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import multiprocessing
 from datetime import datetime
+import pickle
 
 from constants import STARTING_POSITION_OF_AGENT, INF, PROBABILITY_OF_GRID, NUM_ROWS, NUM_COLS, NUM_ITERATIONS
 from helpers.helper import generate_grid_with_probability_p, compute_explored_cells_from_path, \
@@ -59,7 +60,7 @@ def find_the_target(num: int):
 
             agent.execution(random_maze)
 
-            target_found = agent.examine(random_maze, target_pos)
+            target_found = agent.examine(target_pos)
 
             p = 0.0
             for row in range(NUM_ROWS):
@@ -122,6 +123,18 @@ if __name__ == "__main__":
         total_cost_8.append(total_examinations_8[-1] + total_movements_8[-1])
 
     # plot_boxplot([total_cost_6, total_cost_7, total_cost_8], 'boxplot for total cost', legends, 'total_cost.png')
+
+    with open('../data/agent6_100_grids_100x100_flat_target.pkl', 'wb') as f:
+        pickle.dump({'total_actions': total_cost_6, 'total_examinations': total_examinations_6, 'total_movements':
+            total_movements_6}, f)
+
+    with open('../data/agent7_100_grids_100x100_flat_target.pkl', 'wb') as f:
+        pickle.dump({'total_actions': total_cost_7, 'total_examinations': total_examinations_7, 'total_movements':
+            total_movements_7}, f)
+
+    with open('../agent8_100_grids_100x100_flat_target.pkl', 'wb') as f:
+        pickle.dump({'total_actions': total_cost_8, 'total_examinations': total_examinations_8, 'total_movements':
+            total_movements_8}, f)
 
     end_time = time.time()
     print("Average Number of movements of agent 6 = ", np.average(total_movements_6))